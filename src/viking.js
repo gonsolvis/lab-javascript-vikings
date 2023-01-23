83f7d81ed669a1bdc01d0639d2dea315038ee0f8
Ross Gonsolvis <gonsolvis@gmail.com>
2023-01-23 09:34:27 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,98 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+
+    }
+    attack () {
+        return this.strength
+    }
+
+    receiveDamage(thedamage) {
+       this.thedamage = thedamage
+       this.health = this.health - this.thedamage;   
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(name,  health, strength)
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    
+    } 
+
+    attack () {
+        return this.strength
+    }
+    receiveDamage(thedamage) {
+       this.thedamage = thedamage
+       this.health = this.health - this.thedamage; 
+       if (this.health > 0) 
+        return `${this.name} has received ${this.thedamage} points of damage`;
+        else return `${this.name} has died in act of combat`  
+    }
+    
+    battleCry() {
+    return "Odin Owns You All!";}
+    
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    attack () {
+        return this.strength
+    }
+
+    receiveDamage(thedamage) {
+        this.thedamage = thedamage
+        this.health = this.health - this.thedamage; 
+        if (this.health > 0) 
+         return `A Saxon has received ${this.thedamage} points of damage`;
+         else return "A Saxon has died in combat"
+         
+     }
+        
+}
+ 
+
 
 // War
-class War {}
+class War {
+    constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+        
+
+    }
+   
+
+    addViking(vikingObject) {
+        this.vikingArmy.push(vikingObject)
+    }
+    
+    addSaxon(saxonObject) {
+        this.saxonArmy.push(saxonObject)
+    }
+    vikingAttack() {
+    this.vikingArmy(Math.ceiling(Math.random() * this.strength))
+     
+    }
+    saxonAttack() {}
+    showStatus() {}
+
+
+}
+
+/*function selectRandom(arr) {
+    if (arr.length === 0) { return undefined; }
+    let randHolder = Math.floor(Math.random() * arr.length);
+    return arr[randHolder];
+}
+
+*/
